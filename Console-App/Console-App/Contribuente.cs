9637d0612976fbb36cf2409af3f998763d0d9486
Gabriele Bongio <bongiodieci@gmail.com>
2024-02-02 13:06:57 +0100

    Esercizio Finito

---
+++ Console-App/Console-App/Contribuente.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App
+{
+    internal class Contribuente
+    {
+        /*
+            Creazione di get e set per tutte le proprietà richieste
+         */
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public DateTime DataNascita { get; set; }
+        public string CodiceFiscale { get; set; }
+        public char Sesso { get; set; }
+        public string ComuneResidenza { get; set; }
+        public double RedditoAnnuale { get; set; }
+
+        /*
+            metodo costruttore base per poter instanziare il contribuente all'inizio del programma
+            parametri: nessuno
+            return: nessuno
+         */
+        public Contribuente() { }
+
+        /*
+            metodo costruttore per poter inserire tutte le proprietà della classe
+            parametri: i valori da assegnare a tutte le proprietà con relativo tipo
+            return: nessuno
+         */
+        public Contribuente(
+            string nome,
+            string cognome,
+            DateTime dataNascita,
+            string codiceFiscale,
+            char sesso,
+            string comuneResidenza,
+            double redditoAnnuale
+        )
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataNascita = dataNascita;
+            CodiceFiscale = codiceFiscale;
+            Sesso = sesso;
+            ComuneResidenza = comuneResidenza;
+            RedditoAnnuale = redditoAnnuale;
+        }
+
+        /*
+            metodo che calcola l'imposta dovuta dal contribuente in base al reddito
+            parametri: nessuno
+            return: l'imposta dovuta con tipo double
+         */
+        public double ImpostaDovuta()
+        {
+            if (RedditoAnnuale < 15000)
+            {
+                return (RedditoAnnuale / 100) * 23;
+            }
+            else if (RedditoAnnuale < 28000)
+            {
+                double Eccedenza = RedditoAnnuale - 15000;
+                return 3450 + (Eccedenza / 100) * 27;
+            }
+            else if (RedditoAnnuale < 55000)
+            {
+                double Eccedenza = RedditoAnnuale - 28000;
+                return 6960 + (Eccedenza / 100) * 38;
+            }
+            else if (RedditoAnnuale < 75000)
+            {
+                double Eccedenza = RedditoAnnuale - 55000;
+                return 17220 + (Eccedenza / 100) * 41;
+            }
+            else
+            {
+                double Eccedenza = RedditoAnnuale - 75000;
+                return 25420 + (Eccedenza / 100) * 43;
+            }
+        }
+    }
+}
---
+++ Console-App/Console-App/Program.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Console_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Questa parte serve a sistemare la culture e il linguaggio, l'ho usata per stemare la scrittura dei valori di reddito e imposte
+             */
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            var culture = new CultureInfo("fr-FR");
+            culture.NumberFormat.CurrencySymbol = "€";
+            culture.NumberFormat.CurrencyDecimalSeparator = ",";
+            culture.NumberFormat.CurrencyGroupSeparator = ".";
+            culture.NumberFormat.CurrencyPositivePattern = 0;
+            culture.NumberFormat.CurrencyNegativePattern = 2;
+
+            /*
+                Questa parte serve a scrivere l'incipit del codice con il rettangolo e la scritta
+             */
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("/===----====----====----===\\");
+            Console.WriteLine("|                          |");
+            Console.WriteLine("|    Nuovo Contribuente    |");
+            Console.WriteLine("|                          |");
+            Console.WriteLine("\\===----====----====----===/");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("");
+
+            /*
+                Questa parte serve per inserire il nome e renderlo nel formato Nome (cioè prima lettera maiuscola e resto minuscolo), impedisce di lasciare il campo nome vuoto
+             */
+
+            string nomeInput;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Inserisci il tuo nome");
+                Console.ForegroundColor = ConsoleColor.White;
+                nomeInput = Console.ReadLine();
+                Console.WriteLine("");
+                if (nomeInput == "")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Errore, nome non valido");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("");
+                }
+            } while (nomeInput == "");
+
+            string nome = string.Concat(
+                char.ToUpper(nomeInput[0]),
+                nomeInput.Substring(1).ToLower()
+            );
+
+            /*
+                Questa parte serve per inserire il cognome e renderlo nel formato Cognome (cioè prima lettera maiuscola e resto minuscolo), impedisce di lasciare il campo cognome vuoto
+             */
+
+            string cognomeInput;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Inserisci il tuo cognome");
+                Console.ForegroundColor = ConsoleColor.White;
+                cognomeInput = Console.ReadLine();
+                Console.WriteLine("");
+                if (cognomeInput == "")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Errore, cognome non valido");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("");
+                }
+            } while (cognomeInput == "");
+
+            string cognome = string.Concat(
+                char.ToUpper(cognomeInput[0]),
+                cognomeInput.Substring(1).ToLower()
+            );
+
+            /*
+                Questa parte serve per inserire la data con tipo DateTime, c'è una gestione degli errori che fa inserire nuovamente se il formato non è corretto oppure la data è futura
+            */
+
+            bool dataComplete = false;
+            DateTime dataNascita = new DateTime();
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Inserisci la tua data di nascita nel formato (GG/MM/AAAA)");
+                Console.ForegroundColor = ConsoleColor.White;
+                string data = Console.ReadLine();
+                Console.WriteLine("");
+                if (data.Length == 10 && data[2] == '/' && data[5] == '/')
+                {
+                    string[] dettaglidata = data.Split('/');
+                    try
+                    {
+                        int giorno = int.Parse(dettaglidata[0]);
+                        int mese = int.Parse(dettaglidata[1]);
+                        int anno = int.Parse(dettaglidata[2]);
+                        dataNascita = new DateTime(anno, mese, giorno);
+                        if (DateTime.Compare(dataNascita, DateTime.Today) > 0)
+                        {
+                            throw new Exception();
+                        }
+                        dataComplete = true;
+                    }
+                    catch
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(
+                            "Errore nell'inserimento della data di nascita, controlla di non aver inserito lettere, non aver inserito un mese o un giorno non esistente e di non aver inserito una data non ancora avvenuta"
+                        );
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Errore nell'inserimento della data di nascita, riprova");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            } while (!dataComplete);
+
+            /*
+                Questa parte serve per inserire il codice fiscale e renderlo nel formato CODICEFISCALE (cioè tutto con lettere maiuscole), anche questo impedisce di lasciare il campo vuoto
+             */
+
+            string codiceFiscaleInput;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Inserisci il tuo codice fiscale");
+                Console.ForegroundColor = ConsoleColor.White;
+                codiceFiscaleInput = Console.ReadLine();
+                Console.WriteLine("");
+                if (codiceFiscaleInput == "")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Errore, codice fiscale non valido");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("");
+                }
+            } while (codiceFiscaleInput == "");
+
+            string codiceFiscale = codiceFiscaleInput.ToUpper();
+
+            /*
+                Questa parte serve per inserire il sesso, gli unici valori possibili sono 'M' = Maschio, 'F' = Femmina e 'N' = Non Binario
+             */
+
+            char sesso = ' ';
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Inserisci il tuo sesso (M/F/N)");
+                Console.ForegroundColor = ConsoleColor.White;
+                string sessoInput = Console.ReadLine();
+                Console.WriteLine("");
+                if (sessoInput == "M" || sessoInput == "F" || sessoInput == "N")
+                {
+                    sesso = Convert.ToChar(sessoInput);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Errore, sesso non valido");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("");
+                }
+            } while (sesso == ' ');
+
+            /*
+                Questa parte serve per inserire il comonue di residenza e renderlo nel formato Comune (cioè iniziale maiuscola e il resto minuscolo), anche questo impedisce di lasciare il campo vuoto
+             */
+
+            string comuneResidenzaInput;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Inserisci il tuo comune di residenza");
+                Console.ForegroundColor = ConsoleColor.White;
+                comuneResidenzaInput = Console.ReadLine();
+                Console.WriteLine("");
+                if (comuneResidenzaInput == "")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Errore, comune non valido");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("");
+                }
+            } while (comuneResidenzaInput == "");
+
+            string comuneResidenza = string.Concat(
+                char.ToUpper(comuneResidenzaInput[0]),
+                comuneResidenzaInput.Substring(1).ToLower()
+            );
+
+            /*
+                Questa parte serve per inserire il reddito annuale, impedisce di lasciare il campo vuoto e di inserire un numero negativo
+             */
+
+            int redditoAnnuale = -1;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Inserisci il tuo reddito annuale");
+                Console.ForegroundColor = ConsoleColor.White;
+                string redditoAnnualeInput = Console.ReadLine();
+                Console.WriteLine("");
+                try
+                {
+                    redditoAnnuale = int.Parse(redditoAnnualeInput);
+                    if (redditoAnnuale < 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Errore, il reddito annuale non può essere minore di 0");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("");
+                        redditoAnnuale = -1;
+                    }
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Errore, reddito annuale non valido");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("");
+                }
+            } while (redditoAnnuale == -1);
+
+            Contribuente contribuente = new Contribuente(
+                nome,
+                cognome,
+                dataNascita,
+                codiceFiscale,
+                sesso,
+                comuneResidenza,
+                redditoAnnuale
+            );
+            double impostaDaVersare = contribuente.ImpostaDovuta();
+
+            /*
+                Svuoto la console e inserisco l'output richiesto, arrotondando reddito dichiarato e imposta da versare
+             */
+
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("========================================");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("");
+            Console.WriteLine("CALCOLO DELL'IMPOSTA DA VERSARE");
+            Console.WriteLine("");
+            Console.WriteLine($"Contribuente: {contribuente.Nome} {contribuente.Cognome}");
+            Console.WriteLine("");
+            Console.WriteLine(
+                $"Nato il {contribuente.DataNascita.ToShortDateString()} ({contribuente.Sesso})"
+            );
+            Console.WriteLine("");
+            Console.WriteLine($"Residente a {contribuente.ComuneResidenza}");
+            Console.WriteLine("");
+            Console.WriteLine($"Codice fiscale: {contribuente.CodiceFiscale}");
+            Console.WriteLine("");
+            Console.WriteLine(
+                $"Reddito dichiarato: {contribuente.RedditoAnnuale.ToString("C", culture)}"
+            );
+            Console.WriteLine("");
+            Console.WriteLine($"IMPOSTA DA VERSARE: {impostaDaVersare.ToString("C", culture)}");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("========================================");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadLine();
+        }
+    }
+}
